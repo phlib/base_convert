eaaa56ecb1625ea71641b101cbfc3f0394b4d371
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2021-11-27 06:59:22 +0000

    Upgrade PHPUnit to v5
    Allows testing on PHP v7.4 without warnings.

--- tests/BaseConvertTest.php
+++ tests/BaseConvertTest.php
@@ -2,18 +2,20 @@
 
 namespace Phlib;
 
-class BaseConvertTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class BaseConvertTest extends TestCase
 {
     public function testSmallConversionMatchesBuiltinFunction()
     {
         $number = 1234567890;
-        $this->assertEquals(\base_convert($number, 10, 36), \Phlib\base_convert($number, 10, 36));
+        static::assertEquals(\base_convert($number, 10, 36), \Phlib\base_convert($number, 10, 36));
     }
 
     public function testSmallConversionAsString()
     {
         $number = '1234567890';
-        $this->assertEquals(\base_convert($number, 10, 36), \Phlib\base_convert($number, 10, 36));
+        static::assertEquals(\base_convert($number, 10, 36), \Phlib\base_convert($number, 10, 36));
     }
 
     public function testLargeNumberConvertsBack()
@@ -21,7 +23,7 @@ class BaseConvertTest extends \PHPUnit_Framework_TestCase
         $largeNumber = '111222333444555666777888999000';
         $base36 = \Phlib\base_convert($largeNumber, 10, 36);
 
-        $this->assertEquals($largeNumber, \Phlib\base_convert($base36, 36, 10));
+        static::assertEquals($largeNumber, \Phlib\base_convert($base36, 36, 10));
     }
 
     public function testConverts10Correctly()
@@ -29,7 +31,7 @@ class BaseConvertTest extends \PHPUnit_Framework_TestCase
         $largeNumber = '111222333444555666777888999000';
         $base10 = \Phlib\base_convert($largeNumber, 10, 10);
 
-        $this->assertEquals($largeNumber, $base10);
+        static::assertEquals($largeNumber, $base10);
     }
 
     public function testConverts16Correctly()
@@ -37,8 +39,8 @@ class BaseConvertTest extends \PHPUnit_Framework_TestCase
         $largeNumber = '111222333444555666777888999000';
         $hex = \Phlib\base_convert($largeNumber, 10, 16);
 
-        $this->assertEquals('16760f53979875a70ab1f4658', $hex);
-        $this->assertEquals($largeNumber, \Phlib\base_convert($hex, 16, 10));
+        static::assertEquals('16760f53979875a70ab1f4658', $hex);
+        static::assertEquals($largeNumber, \Phlib\base_convert($hex, 16, 10));
     }
 
     public function testConverts36Correctly()
@@ -46,7 +48,7 @@ class BaseConvertTest extends \PHPUnit_Framework_TestCase
         $largeNumber = '111222333444555666777888999000';
         $base36 = \Phlib\base_convert($largeNumber, 10, 36);
 
-        $this->assertEquals('as7078h9iolqwdhaw60', $base36);
-        $this->assertEquals($largeNumber, \Phlib\base_convert($base36, 36, 10));
+        static::assertEquals('as7078h9iolqwdhaw60', $base36);
+        static::assertEquals($largeNumber, \Phlib\base_convert($base36, 36, 10));
     }
 }
