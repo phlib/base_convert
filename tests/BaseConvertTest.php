f1774aba38f704d5fd0bd443fadea20ce7ce69e5
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2021-11-28 07:27:07 +0000

    Add validation for base parameters
    Exceptions for the same deprecation warnings added in PHP v7.4.

--- tests/BaseConvertTest.php
+++ tests/BaseConvertTest.php
@@ -14,6 +14,52 @@ class BaseConvertTest extends TestCase
         static::assertSame(\base_convert($number, 10, 36), \Phlib\base_convert($number, 10, 36));
     }
 
+    /**
+     * @dataProvider dataValidBase
+     */
+    public function testValidFromBase(int $fromBase, bool $isValid): void
+    {
+        if (!$isValid) {
+            $this->expectException(\InvalidArgumentException::class);
+            $this->expectExceptionMessage("Invalid `from base' ({$fromBase})");
+        }
+
+        \Phlib\base_convert('1', $fromBase, 10);
+
+        if ($isValid) {
+            // Valid base should reach this line without error
+            static::assertTrue(true);
+        }
+    }
+
+    /**
+     * @dataProvider dataValidBase
+     */
+    public function testValidToBase(int $toBase, bool $isValid): void
+    {
+        if (!$isValid) {
+            $this->expectException(\InvalidArgumentException::class);
+            $this->expectExceptionMessage("Invalid `to base' ({$toBase})");
+        }
+
+        \Phlib\base_convert('1', 10, $toBase);
+
+        if ($isValid) {
+            // Valid base should reach this line without error
+            static::assertTrue(true);
+        }
+    }
+
+    public function dataValidBase(): array
+    {
+        return [
+            'too-small' => [1, false],
+            'min' => [2, true],
+            'max' => [36, true],
+            'too-large' => [37, false],
+        ];
+    }
+
     public function testLargeNumberConvertsBack(): void
     {
         $largeNumber = '111222333444555666777888999000';
